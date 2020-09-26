8c0e864c9c99a11acc69da4ed0793a2d6de372a0
Jörn Heinemann <joernheinemann@gmx.de>
2020-09-26 17:45:08 +0200

    typescript samples added

---
+++ sample/ts/simple.ts
@@ -0,0 +1,13 @@
+import jsgram from "../../src/index";
+
+
+const app = jsgram();
+
+app.get("/",(req,res)=>{
+	res.send("hello world");
+});
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+app.listen(port, hostname);
