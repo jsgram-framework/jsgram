05cf8fcff1a39f90430c5e29ca907f3937a6cb0a
Jörn Heinemann <joernheinemann@gmx.de>
2020-09-27 18:56:07 +0200

    simple body test update

--- test/Middleware/SimpleBodyTest.ts
+++ test/Middleware/SimpleBodyTest.ts
@@ -38,4 +38,102 @@ describe("SimpleBodyTest",() => {
 				done();
 			});
 	});
+
+	it('should react to the limit', function (done) {
+		const sb = new SimpleBody({
+			limit:1
+		});
+
+		const cb = async (req: ServerRequest ,res: GramResponse) => {
+			let body: string;
+
+			try {
+				body = await sb.read(req);
+			} catch (e) {
+				if(e instanceof Error) {
+					body = e.message;
+				} else if (typeof e === "string") {
+					body = e;
+				}
+			}
+
+			res.send(body);
+		};
+
+		const server = simpleServer(cb);
+
+		const data = "Name";
+
+		chai.request(server)
+			.post('/')
+			.send({
+				"name":data
+			})
+			.end((err, res: Response) => {
+				assert.equal(err,null);
+				assert.equal(res.status,200);
+				assert.equal(res.text,'request entity too large');
+
+				done();
+			});
+	});
+
+	it('should read the body with the middleware', function (done) {
+		const sb = new SimpleBody();
+
+		const cb = (req: ServerRequest ,res: GramResponse) => {
+			sb.process(req,res,async () => {
+				res.send(req.rawBody);
+			});
+		};
+
+		const server = simpleServer(cb);
+
+		const data = "Name";
+
+		chai.request(server)
+			.post('/')
+			.send({
+				"name":data
+			})
+			.end((err, res: Response) => {
+				assert.equal(err,null);
+				assert.equal(res.status,200);
+				assert.equal(res.text,'{"name":"Name"}');
+
+				done();
+			});
+	});
+
+	it('should react to limit errors with the middleware', function (done) {
+		const sb = new SimpleBody({
+			limit:1,
+			encoding:'utf8'
+		});
+
+		const cb = (req: ServerRequest ,res: GramResponse) => {
+			sb.process(req,res,async (err) => {
+				res.send(err);
+			});
+		};
+
+		const server = simpleServer(cb);
+
+		const data = "Name";
+
+		chai.request(server)
+			.post('/')
+			.send({
+				"name":data
+			})
+			.end((err, res: Response) => {
+				assert.equal(err,null);
+				assert.equal(res.status,200);
+				assert.equal(res.text,'request entity too large');
+
+				done();
+			});
+	});
+
+	//TODO edge cases
 });
