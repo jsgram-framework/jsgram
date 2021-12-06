49769eac30c1cda5703d423a0a5ac0da84b83a74
Jörn Heinemann <joernheinemann@gmx.de>
2021-12-06 13:06:41 +0100

    format tests with es lint

--- test/App/QueueTest.ts
+++ test/App/QueueTest.ts
@@ -1,6 +1,6 @@
 import {Middleware, NextFunction} from "../../src";
 
-let chai = require('chai');
+const chai = require("chai");
 
 import {assert} from "chai";
 import chaiHttp = require("chai-http");
@@ -14,211 +14,211 @@ import {Queue} from "../../src/App/Queue";
 
 chai.use(chaiHttp);
 
-function createQueue(extraMw: Middleware[] = [], queueKey: string = "queue")
+function createQueue(extraMw: Middleware[] = [], queueKey = "queue")
 {
 	const mw: Middleware[] = [
-		(req: ServerRequest ,res: GramResponse, next: NextFunction) => {
-			req.setAttribute(queueKey,"mw1");
+		(req: ServerRequest, res: GramResponse, next: NextFunction) => {
+			req.setAttribute(queueKey, "mw1");
 
 			next();
 		},
-		(req: ServerRequest ,res: GramResponse, next: NextFunction) => {
+		(req: ServerRequest, res: GramResponse, next: NextFunction) => {
 			const string = req.getAttribute(queueKey);
 
-			req.setAttribute(queueKey,string+"mw2");
+			req.setAttribute(queueKey, string + "mw2");
 
 			next();
 		},
-		(req: ServerRequest ,res: GramResponse, next: NextFunction) => {
+		(req: ServerRequest, res: GramResponse, next: NextFunction) => {
 			const string = req.getAttribute(queueKey);
 
-			req.setAttribute(queueKey,string+"mw3");
+			req.setAttribute(queueKey, string + "mw3");
 
 			next();
-		},
+		}
 	];
 
 	mw.push( ...extraMw);	//pushing the extra middleware
 
-	const last = async (req: ServerRequest ,res: GramResponse) => {
+	const last = async (req: ServerRequest, res: GramResponse) => {
 		res.send(req.getAttribute(queueKey));
 	};
 
-	return new Queue(mw,last);
+	return new Queue(mw, last);
 }
 
-describe("QueueTest",() => {
-	it('should work without middleware', function (done) {
-		const last = async (req: ServerRequest ,res: GramResponse) => {
+describe("QueueTest", () => {
+	it("should work without middleware", function(done) {
+		const last = async (req: ServerRequest, res: GramResponse) => {
 			res.send("worked");
 		};
 
-		const queue = new Queue([],last);
+		const queue = new Queue([], last);
 
-		const cb = async (req: ServerRequest ,res: GramResponse) => {
-			await queue.handle(req,res);
+		const cb = async (req: ServerRequest, res: GramResponse) => {
+			await queue.handle(req, res);
 		};
 
 		const server = simpleServer(cb);
 
 		chai.request(server)
-			.get('/')
+			.get("/")
 			.end((err, res: Response) => {
-				assert.equal(err,null);
-				assert.equal(res.status,200);
-				assert.equal(res.text,"worked");
+				assert.equal(err, null);
+				assert.equal(res.status, 200);
+				assert.equal(res.text, "worked");
 
 				done();
 			});
 	});
 
-	it('should invoke simple middleware', function (done) {
+	it("should invoke simple middleware", function(done) {
 		const queue = createQueue();
 
-		const cb = async (req: ServerRequest ,res: GramResponse) => {
-			await queue.handle(req,res);
+		const cb = async (req: ServerRequest, res: GramResponse) => {
+			await queue.handle(req, res);
 		};
 
 		const server = simpleServer(cb);
 
 		chai.request(server)
-			.get('/')
+			.get("/")
 			.end((err, res: Response) => {
-				assert.equal(err,null);
-				assert.equal(res.status,200);
-				assert.equal(res.text,"mw1mw2mw3");
+				assert.equal(err, null);
+				assert.equal(res.status, 200);
+				assert.equal(res.text, "mw1mw2mw3");
 
 				done();
 			});
 	});
 
-	it('should react to errors from middleware with string', function (done) {
+	it("should react to errors from middleware with string", function(done) {
 		//string error
 		const queue = createQueue([
-			(req: ServerRequest ,res: GramResponse, next: NextFunction) => {
+			(req: ServerRequest, res: GramResponse, next: NextFunction) => {
 				next("Error test");
 			}
 		]);
 
-		const cb = async (req: ServerRequest ,res: GramResponse) => {
-			await queue.handle(req,res);
+		const cb = async (req: ServerRequest, res: GramResponse) => {
+			await queue.handle(req, res);
 		};
 
 		const server = simpleServer(cb);
 
 		chai.request(server)
-			.get('/')
+			.get("/")
 			.end((err, res: Response) => {
-				assert.equal(err,null);
-				assert.equal(res.status,500);
-				assert.equal(res.text,"Error test");
+				assert.equal(err, null);
+				assert.equal(res.status, 500);
+				assert.equal(res.text, "Error test");
 
 				done();
 			});
 	});
 
-	it('should react to errors from middleware as callback and ending response', function (done) {
-		const errorCallback = (req: ServerRequest ,res: GramResponse) => {
+	it("should react to errors from middleware as callback and ending response", function(done) {
+		const errorCallback = (req: ServerRequest, res: GramResponse) => {
 			res.send("Error test");
 		};
 
 		//cb error
 		const queue = createQueue([
-			(req: ServerRequest ,res: GramResponse, next: NextFunction) => {
+			(req: ServerRequest, res: GramResponse, next: NextFunction) => {
 				next(errorCallback);
 			}
 		]);
 
-		const cb = async (req: ServerRequest ,res: GramResponse) => {
-			await queue.handle(req,res);
+		const cb = async (req: ServerRequest, res: GramResponse) => {
+			await queue.handle(req, res);
 		};
 
 		const server = simpleServer(cb);
 
 		chai.request(server)
-			.get('/')
+			.get("/")
 			.end((err, res: Response) => {
-				assert.equal(err,null);
-				assert.equal(res.status,500);
-				assert.equal(res.text,"Error test");
+				assert.equal(err, null);
+				assert.equal(res.status, 500);
+				assert.equal(res.text, "Error test");
 
 				done();
 			});
 	});
 
-	it('should react to errors from middleware as callback without ending response', function (done) {
-		const errorCallback = (req: ServerRequest ,res: GramResponse) => {
+	it("should react to errors from middleware as callback without ending response", function(done) {
+		const errorCallback = (req: ServerRequest, res: GramResponse) => {
 			res.write("Error test");
 		};
 
 		//cb error
 		const queue = createQueue([
-			(req: ServerRequest ,res: GramResponse, next: NextFunction) => {
+			(req: ServerRequest, res: GramResponse, next: NextFunction) => {
 				next(errorCallback);
 			}
 		]);
 
-		const cb = async (req: ServerRequest ,res: GramResponse) => {
-			await queue.handle(req,res);
+		const cb = async (req: ServerRequest, res: GramResponse) => {
+			await queue.handle(req, res);
 		};
 
 		const server = simpleServer(cb);
 
 		chai.request(server)
-			.get('/')
+			.get("/")
 			.end((err, res: Response) => {
-				assert.equal(err,null);
-				assert.equal(res.status,500);
-				assert.equal(res.text,"Error test");
+				assert.equal(err, null);
+				assert.equal(res.status, 500);
+				assert.equal(res.text, "Error test");
 
 				done();
 			});
 	});
 
-	it('should react to errors from middleware when the res is already closed', function (done) {
+	it("should react to errors from middleware when the res is already closed", function(done) {
 		const queue = createQueue([
-			(req: ServerRequest ,res: GramResponse, next: NextFunction) => {
+			(req: ServerRequest, res: GramResponse, next: NextFunction) => {
 				res.send("Res closed");
 				next("Error test");
 			}
 		]);
 
-		const cb = async (req: ServerRequest ,res: GramResponse) => {
-			await queue.handle(req,res);
+		const cb = async (req: ServerRequest, res: GramResponse) => {
+			await queue.handle(req, res);
 		};
 
 		const server = simpleServer(cb);
 
 		chai.request(server)
-			.get('/')
+			.get("/")
 			.end((err, res: Response) => {
-				assert.equal(err,null);
-				assert.equal(res.status,200);
-				assert.equal(res.text,"Res closed");
+				assert.equal(err, null);
+				assert.equal(res.status, 200);
+				assert.equal(res.text, "Res closed");
 
 				done();
 			});
 	});
 
-	it('should react to errors from middleware with another status', function (done) {
+	it("should react to errors from middleware with another status", function(done) {
 		const queue = createQueue([
-			(req: ServerRequest ,res: GramResponse, next: NextFunction) => {
-				next("Error test",413);
+			(req: ServerRequest, res: GramResponse, next: NextFunction) => {
+				next("Error test", 413);
 			}
 		]);
 
-		const cb = async (req: ServerRequest ,res: GramResponse) => {
-			await queue.handle(req,res);
+		const cb = async (req: ServerRequest, res: GramResponse) => {
+			await queue.handle(req, res);
 		};
 
 		const server = simpleServer(cb);
 
 		chai.request(server)
-			.get('/')
+			.get("/")
 			.end((err, res: Response) => {
-				assert.equal(err,null);
-				assert.equal(res.status,413);
-				assert.equal(res.text,"Error test");
+				assert.equal(err, null);
+				assert.equal(res.status, 413);
+				assert.equal(res.text, "Error test");
 
 				done();
 			});
--- test/Util/ResponseTest.ts
+++ test/Util/ResponseTest.ts
@@ -1,6 +1,6 @@
 import {assert} from "chai";
 
-let chai = require('chai');
+const chai = require("chai");
 
 import chaiHttp = require("chai-http");
 import {Response} from "superagent";
@@ -11,78 +11,78 @@ import {ServerRequest} from "../../src";
 
 chai.use(chaiHttp);
 
-describe("ResponseTest",() => {
-	it('should return a string with text', function (done) {
-		const cb = async (req: ServerRequest ,res: GramResponse) => {
-			res.text("Test")
+describe("ResponseTest", () => {
+	it("should return a string with text", function(done) {
+		const cb = async (req: ServerRequest, res: GramResponse) => {
+			res.text("Test");
 		};
 
 		const server = simpleServer(cb);
 
 		chai.request(server)
-			.get('/')
+			.get("/")
 			.end((err, res: Response) => {
-				assert.equal(err,null);
-				assert.equal(res.status,200);
-				assert.equal(res.text,"Test");
-				assert.equal(res.header['content-type'],"text/plaintext");
+				assert.equal(err, null);
+				assert.equal(res.status, 200);
+				assert.equal(res.text, "Test");
+				assert.equal(res.header["content-type"], "text/plaintext");
 
 				done();
 			});
 	});
 
-	it('should return a string with json', function (done) {
-		const cb = async (req: ServerRequest ,res: GramResponse) => {
-			res.json({test:"test"})
+	it("should return a string with json", function(done) {
+		const cb = async (req: ServerRequest, res: GramResponse) => {
+			res.json({test: "test"});
 		};
 
 		const server = simpleServer(cb);
 
 		chai.request(server)
-			.get('/')
+			.get("/")
 			.end((err, res: Response) => {
-				assert.equal(err,null);
-				assert.equal(res.status,200);
-				assert.equal(res.text,"{\"test\":\"test\"}");
-				assert.equal(res.header['content-type'],"application/json");
+				assert.equal(err, null);
+				assert.equal(res.status, 200);
+				assert.equal(res.text, "{\"test\":\"test\"}");
+				assert.equal(res.header["content-type"], "application/json");
 
 				done();
 			});
 	});
 
-	it('should return a string with send', function (done) {
-		const cb = async (req: ServerRequest ,res: GramResponse) => {
-			res.send("Test")
+	it("should return a string with send", function(done) {
+		const cb = async (req: ServerRequest, res: GramResponse) => {
+			res.send("Test");
 		};
 
 		const server = simpleServer(cb);
 
 		chai.request(server)
-			.get('/')
+			.get("/")
 			.end((err, res: Response) => {
-				assert.equal(err,null);
-				assert.equal(res.status,200);
-				assert.equal(res.text,"Test");
-				assert.equal(res.header['content-type'],"text/html");
+				assert.equal(err, null);
+				assert.equal(res.status, 200);
+				assert.equal(res.text, "Test");
+				assert.equal(res.header["content-type"], "text/html");
 
 				done();
 			});
 	});
 
-	it('should return a string from json with send', function (done) {
-		const cb = async (req: ServerRequest ,res: GramResponse) => {
-			res.send({test:"test"})
+	it("should return a string from json with send", function(done) {
+		const cb = async (req: ServerRequest, res: GramResponse) => {
+			res.send({test: "test"});
 		};
 
 		const server = simpleServer(cb);
 
 		chai.request(server)
-			.get('/')
+			.get("/")
 			.end((err, res: Response) => {
-				assert.equal(err,null);
-				assert.equal(res.status,200);
-				assert.equal(res.text,"{\"test\":\"test\"}");
-				assert.equal(res.header['content-type'],"application/json");
+				assert.equal(err, null);
+				assert.equal(res.status, 200);
+				assert.equal(res.text, "{\"test\":\"test\"}");
+				assert.equal(res.header["content-type"], "application/json");
 
 				done();
 			});
